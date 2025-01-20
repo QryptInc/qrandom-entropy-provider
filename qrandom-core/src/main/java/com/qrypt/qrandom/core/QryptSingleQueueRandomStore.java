d6154691919d05ce51aaf7bd3591d948a8fa60be
mikhail.zingman <mikhail.zingman@gmail.com>
2025-01-20 18:14:48 +0000

    fixing exposed api key

--- qrandom-core/src/main/java/com/qrypt/qrandom/core/QryptSingleQueueRandomStore.java
+++ qrandom-core/src/main/java/com/qrypt/qrandom/core/QryptSingleQueueRandomStore.java
@@ -15,8 +15,6 @@ import java.util.function.Function;
 public class QryptSingleQueueRandomStore implements RandomStore {
     private static final Logger logger = LoggerFactory.getLogger(QryptSingleQueueRandomStore.class.getName());
 
-    private static final String DEFAULT_API_URL = "https://api-eus.qrypt.com/api/v1/entropy";
-    private static final String DEFAULT_TOKEN = "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImY0NjEzODdkOTg2ZjQ2OTliOTQzOGI5MTA1MTYwYTliIn0.eyJleHAiOjE3NTUxOTM5NjUsIm5iZiI6MTcyMzY1Nzk2NSwiaXNzIjoiQVVUSCIsImlhdCI6MTcyMzY1Nzk2NSwiZ3JwcyI6WyJQVUIiXSwiYXVkIjpbIlJQUyJdLCJybHMiOlsiUk5EVVNSIl0sImNpZCI6IjBqX2N0cFF3UW9YT0NkLVhaeEZvRiIsImR2YyI6IjNlM2NlZTBlYjVlMDRjNmZiNjM0OWViZDIxNjFmNGE1IiwianRpIjoiMzM5ZWMzNmVkMTlmNGE2YWI3ZWZkMTFiNGI1YzcxMWMiLCJ0eXAiOjN9.Tr_0vh4u0GpnRUFYjsy0Adg_VckMrhssrzfCrS9wmjNZ6PSk8B0xhinO4TCIKVW3xYn7ztssthmWYCj-pA3_NA";
     private static final int DEFAULT_STORE_SIZE = 1000;
     private static final int DEFAULT_MIN_THRESHOLD = 200;
 
@@ -36,6 +34,10 @@ public class QryptSingleQueueRandomStore implements RandomStore {
     private static <T> T getSystemProperty(String property, T defaultValue, Function<String,T> converter) {
         String value = System.getProperty(property);
         if (value == null) {
+            if (defaultValue == null) {
+                //need to throw proper exception when system property is not defined and no default value exist
+                throw new IllegalArgumentException("Property "+property+" is not defined anywhere and no default value specified");
+            }
             return defaultValue;
         }
 
@@ -55,8 +57,8 @@ public class QryptSingleQueueRandomStore implements RandomStore {
             //thread safety
             synchronized (QryptSingleQueueRandomStore.class) {
                 if (instance == null) {
-                    String apiUrl = getSystemProperty("qrypt.api.url", DEFAULT_API_URL, Function.identity());
-                    String token = getSystemProperty("qrypt.api.token",DEFAULT_TOKEN,Function.identity());
+                    String apiUrl = getSystemProperty("qrypt.api.url", null, Function.identity());
+                    String token = getSystemProperty("qrypt.api.token",null,Function.identity());
                     int storeSize = getSystemProperty("qrypt.store.size",DEFAULT_STORE_SIZE, Integer::valueOf);
                     int storeMinThreshold = getSystemProperty("qrypt.store.min_threshold",DEFAULT_MIN_THRESHOLD,Integer::valueOf);
 
